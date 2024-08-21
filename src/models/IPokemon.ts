16b82d99d7fc35168dd8561333eb475d6eb97ea6
YaroslavHunt <yaroslavohotskiy@gmail.com>
2024-08-21 17:14:14 +0300

    Add pokemon interface in models/IPokemon.ts

---
+++ src/models/IPokemon.ts
@@ -0,0 +1,40 @@
+export interface Pokemon {
+    id: number;
+    name: string;
+    abilities: Ability[];
+    stats: Stat[];
+    sprites: Sprite;
+    forms: Form[];
+}
+
+interface Ability {
+    ability: {
+        name: string,
+        url: string
+    },
+    is_hidden: boolean,
+    slot: number
+}
+
+interface Stat {
+    base_stat: number,
+    effort: number,
+    stat: {
+        name: string,
+        url: string
+    }
+}
+
+interface Sprite {
+    other: {
+        dream_world: {
+            front_default: string;
+        }
+    }
+}
+
+interface Form {
+    name: string,
+    url: string
+}
+
